918e6d79b4d2e90708bebf11f258cdb6ae0d1b38
Arnavghimire <ghimirearnav738@gmail.com>
2025-03-18 12:06:19 +0545

    add it

--- que8.py
+++ que8.py
@@ -1,3 +1,8 @@
+#8)	Give an appropriate if statement for each of the following
+#(the value of num is not important):
+#(a) Displays 'within range' if num is between 0 and 100, inclusive.
+#(b) Displays 'within range' if num is between 0 and 100, inclusive, and displays 'out of range' otherwise.
+
 num=int(input("Enter a number: "))
 if 0<=num<=100:
     print("within range")
